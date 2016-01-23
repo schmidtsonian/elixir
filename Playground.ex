ec91d1836e278ac485719f98a6c224063466f437
Esteban Ramirez <eramirez@grupow.com>
2016-01-22 19:11:27 -0600

    Fibbonacci alert negative numbers

--- Playground.ex
+++ Playground.ex
@@ -1,8 +1,11 @@
 defmodule Playground do
 	def fib(0), do: 1
 	def fib(1), do: 1
-	def fib(n) do
+	def fib(n) when n > 1 do
 		fib(n-2) + fib(n-1)
+	end
+	def fib(n) when n < 0 do
+		IO.puts "Fibonacci not supported"
 	end
 
 end
